deb947cf39bda3e333a0ff93b84835997f7652e9
Sarah Osborn <sarah.osborn0421@gmail.com>
2019-10-04 17:55:26 -0400

    Hot Air Balloon class definition
    The initial structure of the Skateboard class was implemented, including member data and
    functions.

---
+++ HotAirBalloon.cpp
@@ -0,0 +1,33 @@
+//  Created by Sarah Osborn on 10/3/19.
+
+#include "HotAirBalloon.h"
+
+HotAirBalloon::HotAirBalloon()
+{
+  windSpeed = 0;
+}
+
+HotAirBalloon::HotAirBalloon(string brand, string model, string fuelType, int windSpeed)
+{
+}
+
+HotAirBalloon::~HotAirBalloon() = default;
+
+int HotAirBalloon::getWindSpeed()
+{
+  return 0;
+}
+
+void HotAirBalloon::setWindSpeed(int ws)
+{
+}
+
+double HotAirBalloon::mileageEstimate(double time)
+{
+  return 0;
+}
+
+string HotAirBalloon::toString()
+{
+  return "";
+}
